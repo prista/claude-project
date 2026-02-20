4a2c5d37ab119e7fd0a163363ccb68392a430615
prista <games.prista@mail.ru>
2026-02-20 17:28:01 +0300

    Add nextCookies() plugin to lib/auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
 import { db } from "./db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: db,
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()] // make sure this is the last plugin in the array
 });
